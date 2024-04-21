2a12a0cd7a902cdab33643b9a4444091c0eb8bc2
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-21 10:49:18 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -19,13 +19,13 @@ VALIDATE() {
     fi
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
-fi
+    if [ $USERID -ne 0 ]
+    then
+     echo "Please run this script with root access."
+     exit 1 # manually exit if error comes.
+    else
+     echo "You are super user."
+    fi
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $?"iNSTALLING my sql server "

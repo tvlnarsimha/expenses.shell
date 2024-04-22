d113fcd154a996c729905216da41e4146afb705f
tvlnarsimha <tvlnarsimha@gmail.com>
2024-04-22 13:19:00 +0530

    functions

---
+++ mysql.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+    then
+      echo -e "$2 ...$R Failure $N"
+      exit 1
+    else
+     echo -e "$2...$G Success $N"
+   fi
+}
+
+if [ $USERID -ne 0 ]
+then
+ echo "Please run this script with root access"
+ exit 1 # manually exit if error comes
+else
+ echo "you are the super user"
+fi
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySql Server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabling Mysql server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Start Mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting up the root password"
+
+
